d44c025ddc5ef76613ecb88f725b9cf81a638d14
nowmaciej <now.maciej@gmail.com>
2022-10-29 08:33:37 +0200

    second push

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+X
+(src/main/java/org/coderslab/database.csv,2/b/2b60ebf2fd85e2816a5903d4fd49191e865b8b9c
+U
+%src/main/java/org/coderslab/Main.java,a/a/aa75391f5e4ffada8c666449bd24b8cd4b9b1e00
--- src/main/java/org/coderslab/Main.java
+++ src/main/java/org/coderslab/Main.java
@@ -1,7 +1,106 @@
 package org.coderslab;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class Main {
+
+    public static final String DATABASE_FILE = "database.csv";
+
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String RED_UNDERLINED = "\033[4;31m";    // RED
+    public static final String RESET = "\033[0m";  // Text Reset
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainMenu();
+    }
+
+    public static void mainMenu() {
+        System.out.println("Please select an option:");
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+
+        Scanner scan = new Scanner(System.in);
+        boolean inMenu = true;
+
+        while (inMenu) {
+            switch (scan.next()) {
+                case "add":
+                    addMenu();
+                    inMenu = false;
+                    break;
+                case "remove":
+                    removeMenu();
+                    inMenu = false;
+                    break;
+                case "list":
+                    listMenu();
+                    inMenu = false;
+                    break;
+                case "exit":
+                    exitOption();
+                    inMenu = false;
+                    break;
+                default:
+                    System.out.println("You need to choose an action!");
+            }
+        }
+    }
+
+    private static void exitOption() {
+        System.out.println("Thank you!\nSee you next time");
+    }
+
+    private static void listMenu() {
+        File dbFile = new File(DATABASE_FILE);
+
+        try (Scanner scan = new Scanner(dbFile)) {
+            int i = 0;
+            while (scan.hasNextLine()) {
+                System.out.println(i + ": " + scan.nextLine());
+                i++;
+            }
+        } catch (IOException e) {
+            System.out.println("Can't load file " + DATABASE_FILE);
+        }
+    }
+
+    private static void removeMenu() {
+        System.out.println("you are in remove");
+
+    }
+
+    private static void addMenu() {
+        try (FileWriter fw = new FileWriter(DATABASE_FILE, true)) {
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Now adding task to database");
+            System.out.print("Enter task name: ");
+            String taskName = scan.nextLine();
+            System.out.print("Enter task date [yyyy-mm-dd]: ");
+            String taskDate = scan.nextLine();
+            System.out.print("Enter task status [true/false]: ");
+            boolean taskStatus = scan.nextBoolean();
+
+
+            String newLine = taskName + ", " + taskDate + ", " + taskStatus;
+            fw.append(newLine + "\n");
+
+            System.out.println("Added new task: " + newLine);
+
+        } catch (IOException e) {
+            System.out.println("File " + DATABASE_FILE + " not found!");
+        }
+
+        mainMenu();
+
     }
 }
